c8c9d2703a0b1722e182f1cae721f4f5aa178d4a
IDSILA86 <idsila87@gmail.com>
2024-11-03 22:15:45 +0400

    update server.js

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const sleep = ms => new Promise(res => setTimeout(res, ms));
 app.get('/server', async (req, res) => {
   const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, 'webdriver', { get: () => false });
+  });
   await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
   await page.setViewport({ width: 375, height: 812 });
   await page.goto(`https://${req.query.host}/`);
